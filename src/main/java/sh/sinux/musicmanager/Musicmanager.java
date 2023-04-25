f2ad4ad027374ddc847de3e170c606991d564458
jay <97389304+coolmanks11@users.noreply.github.com>
2023-04-25 15:35:30 +0100

    chore: support netbeans
    Mind CRFL/LF

---
+++ src/main/java/sh/sinux/musicmanager/Musicmanager.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package sh.sinux.musicmanager;
+
+/**
+ *
+ * @author ZiJieLee
+ */
+public class Musicmanager {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
