6185ae1ba6865d85ccc4b7229a242e9a32f9cdf4
Simon LEONARD <git-1001af4@sinux.sh>
2023-04-27 14:54:25 +0100

    feat: interface for LibraryStorage

---
+++ src/main/java/sh/sinux/musicmanager/LibraryStorage/LibraryStorage.java
@@ -0,0 +1,24 @@
+package sh.sinux.musicmanager.LibraryStorage;
+
+import sh.sinux.musicmanager.Song.Song;
+
+public interface LibraryStorage {
+    void add(Song song);
+
+    /**
+     * Removes the song from the library storage
+     * @param song
+     */
+    void remove(Song song);
+    void remove(int trackNumber);
+
+    /**
+     * Updates the song in the library storage
+     * The track number is used to find the song to update
+     */
+    void update(Song song);
+    Song get(int trackNumber);
+    Song findOneByTitle(String title);
+    Song findOneByAlbum(String album);
+    Song findOneByArtist(String artist);
+}
