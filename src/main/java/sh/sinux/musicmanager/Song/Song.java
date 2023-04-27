dcb9ec65cfff3f56f65794c51f94db94ad21768e
Simon LEONARD <git-1001af4@sinux.sh>
2023-04-27 14:54:10 +0100

    feat: class song and its comparator

---
+++ src/main/java/sh/sinux/musicmanager/Song/Song.java
@@ -0,0 +1,80 @@
+package sh.sinux.musicmanager.Song;
+
+public class Song implements Comparable<Song> {
+    private static int NEXT_TRACK_NUMBER = 0;
+    private int trackNumber;
+    private String title;
+    private String artist;
+    private String album;
+    private String genre;
+    /**
+     * Play count is the number of times the song has been played
+     */
+    private int playCount;
+    /**
+     * Rating is a float between 0.0 and 5.0
+     */
+    private float rating;
+
+    /// Constructors ///
+
+    /**
+     * Public constructor with essential parameters
+     * trackNumber is automatically generated
+     * playCount and rating are set to 0
+     */
+    public Song(String title, String album, String artist, String genre) {
+        this(NEXT_TRACK_NUMBER++, title, album, artist, genre, 0, 0.0f);
+    }
+
+    /**
+     * Private constructor with all parameters
+     */
+    private Song(int trackNumber, String title, String album, String artist, String genre, int playCount, float rating) {
+        this.trackNumber = trackNumber;
+        this.title = title;
+        this.album = album;
+        this.artist = artist;
+        this.genre = genre;
+        this.playCount = playCount;
+        this.rating = rating;
+    }
+
+    /// Methods ///
+
+    /**
+     * Fake method to simulate playing a song
+     * Increments playCount by 1
+     */
+    public void incrementPlayCount() {
+        playCount++;
+    }
+
+    /**
+     * The natural order for Songs is by title (ASCENDING), and where two titles are the same, by artist (ASCENDING).
+     * @param other the other song to compare to
+     * @return -1 if this song is less than the other song, 0 if they are equal, 1 if this song is greater than the other song
+     */
+    @Override
+    public int compareTo(Song other) {
+        if (title.equals(other.title))
+            return artist.compareTo(other.artist);
+        return title.compareTo(other.title);
+    }
+
+    /// Getters ///
+
+    public int getTrackNumber() { return trackNumber; }
+
+    public String getTitle() { return title; }
+
+    public String getAlbum() { return album; }
+
+    public String getArtist() { return artist; }
+
+    public String getGenre() { return genre; }
+
+    public int getPlayCount() { return playCount; }
+
+    public float getRating() { return rating; }
+}
---
+++ src/main/java/sh/sinux/musicmanager/Song/comparators/GenreComparator.java
@@ -0,0 +1,17 @@
+package sh.sinux.musicmanager.Song.comparators;
+
+import sh.sinux.musicmanager.Song.Song;
+
+import java.util.Comparator;
+
+public class GenreComparator implements Comparator<Song> {
+    /**
+     * Compares two songs by genre, and where two genres are the same, use natural order (by title and artist)
+     */
+    @Override
+    public int compare(Song song1, Song song2) {
+        if (song1.getGenre().equals(song2.getGenre()))
+            return song1.compareTo(song2);
+        return song1.getGenre().compareTo(song2.getGenre());
+    }
+}
