12d44d0526ae0c4ff48e03871462461dbfe94ace
jay <97389304+coolmanks11@users.noreply.github.com>
2023-04-30 16:09:39 +0100

    feat/LibraryStorageLinkedList

--- src/main/java/sh/sinux/musicmanager/LibraryStorageLinkedList/LibraryStorageLinkedList.java
+++ src/main/java/sh/sinux/musicmanager/LibraryStorageLinkedList/LibraryStorageLinkedList.java
@@ -4,6 +4,9 @@ import sh.sinux.musicmanager.LibraryStorage.LibraryStorage;
 import sh.sinux.musicmanager.MyLinkedList.MyLinkedList;
 import sh.sinux.musicmanager.Song.Song;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class LibraryStorageLinkedList implements LibraryStorage {
     MyLinkedList<Song> songs = new MyLinkedList<Song>();
 
@@ -14,12 +17,21 @@ public class LibraryStorageLinkedList implements LibraryStorage {
 
     @Override
     public void remove(Song song) {
-
+        songs.remove(song);
     }
 
     @Override
     public void remove(int trackNumber) {
-
+        Song toRemove = null;
+        for (Song song : songs) {
+            if (song.getTrackNumber() == trackNumber) {
+                toRemove = song;
+                break;
+            }
+        }
+        if (toRemove != null) {
+            songs.remove(toRemove);
+        }
     }
 
     @Override
@@ -29,21 +41,45 @@ public class LibraryStorageLinkedList implements LibraryStorage {
 
     @Override
     public Song get(int trackNumber) {
+        for (Song song : songs) {
+            if (song.getTrackNumber() == trackNumber) {
+                return song;
+            }
+        }
         return null;
     }
 
     @Override
     public Song[] searchByTitle(String title) {
-        return new Song[0];
+        List<Song> matchingSongs = new ArrayList<>();
+        for (Song song : songs) {
+            if (song.getTitle().equalsIgnoreCase(title)) {
+                matchingSongs.add(song);
+            }
+        }
+        return matchingSongs.toArray(new Song[matchingSongs.size()]);
     }
 
     @Override
     public Song[] searchByAlbum(String album) {
-        return new Song[0];
+        List<Song> matchingSongs = new ArrayList<>();
+        for (Song song : songs) {
+            if (song.getAlbum().equalsIgnoreCase(album)) {
+                matchingSongs.add(song);
+            }
+        }
+        return matchingSongs.toArray(new Song[matchingSongs.size()]);
     }
 
     @Override
     public Song[] searchByArtist(String artist) {
-        return new Song[0];
+        List<Song> matchingSongs = new ArrayList<>();
+        for (Song song : songs) {
+            if (song.getArtist().equalsIgnoreCase(artist)) {
+                matchingSongs.add(song);
+            }
+        }
+        return matchingSongs.toArray(new Song[matchingSongs.size()]);
     }
+
 }
